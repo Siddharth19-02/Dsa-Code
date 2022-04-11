6813a51ff37841364661fef6248a3a97839cdc0e
Siddharth19-02 <71065965+Siddharth19-02@users.noreply.github.com>
2022-04-11 23:38:56 +0530

    Create 636-exclusive-time-of-functions.cpp

---
+++ Stack/636-exclusive-time-of-functions.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        
+        vector<int>vec(n,0);
+        stack<pair<int,int> >s;
+        int lastExecutableTime = 0;
+        
+        for(int i=0;i<logs.size();i++)
+        {
+            string curr = logs[i];
+            bool flag = false;
+            int j = 0;
+            int startingNumber =0;
+            int endingNumber =0;
+            while(j<curr.length() and curr[j]!=':')
+            {
+                startingNumber = startingNumber*10 + (curr[j]-'0');
+                j++;
+            }
+            j++;
+            
+            if(curr[j]=='s')
+            {
+                flag = true;
+            }
+            
+            while(j<curr.length() and curr[j]!=':')
+            {
+                j++;
+            }
+            j++;
+            
+            while(j<curr.length())
+            {
+                endingNumber = endingNumber*10 + (curr[j]-'0');
+                j++;
+            }
+            
+            if(flag)
+            {
+                if(!s.empty())
+                {
+                    int cu = s.top().first;
+                    vec[cu] += endingNumber - lastExecutableTime ;
+                    lastExecutableTime = endingNumber;
+                }
+                s.push({startingNumber,endingNumber});
+            }
+            else
+            {
+                int first = s.top().first;
+                int second= s.top().second;
+                
+                vec[first] += endingNumber - lastExecutableTime +1;
+                s.pop();
+                lastExecutableTime  = endingNumber+1;
+            }
+            
+        }
+        return vec;
+    }
+};
