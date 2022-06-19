772d5eb7415a8ab9269856d56a90cb3ddf2a93db
Siddharth19-02 <71065965+Siddharth19-02@users.noreply.github.com>
2022-06-19 11:34:07 +0530

    Create 1567-maximum-length-of-subarray-with-positive-product.cpp

---
+++ Dynamic Programming/1567-maximum-length-of-subarray-with-positive-product.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int positive = 0, negative = 0;
+        int ans = 0;
+
+        for(int x : nums)
+        {
+            if(x == 0)
+            {
+                positive = 0;
+                negative = 0;
+            }
+            else if(x > 0)
+            {
+                positive++;
+                negative = negative == 0 ? 0  : negative+1;
+            }
+            else 
+            {
+                int temp = positive;
+                positive = negative == 0 ? 0  : negative+1;
+                negative = temp+1;
+            }
+            ans = max(ans, positive);
+        }
+        return ans;
+    }
+};
