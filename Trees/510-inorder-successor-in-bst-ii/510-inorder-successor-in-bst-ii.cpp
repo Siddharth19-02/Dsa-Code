31ab411a2933a7efa3c4458134ac559968b06c92
Siddharth19-02 <71065965+Siddharth19-02@users.noreply.github.com>
2022-11-27 10:36:47 +0530

    Create 510-inorder-successor-in-bst-ii.cpp

---
+++ Trees/510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    Node* inorderSuccessor(Node* node) {
+        if(node->right)
+        {
+            node = node->right;
+            while(node->left)
+            {
+                node = node->left;
+            }
+            return node;
+        }
+        int val = node->val;
+        while(node->parent)
+        {
+            if(node->parent->val>val)
+            {
+                break;
+            }
+            node = node->parent;
+        }
+        
+        return node->parent;
+    }
+};
