ecbdf4ea27885c443c0301ee6b954a6cc4e39697
Siddharth19-02 <71065965+Siddharth19-02@users.noreply.github.com>
2023-05-11 12:51:57 +0530

    Create 1035-uncrossed-lines.cpp

---
+++ Dynamic Programming/1035-uncrossed-lines.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        
+        int n = nums1.size();
+        int m = nums2.size();
+        
+        int dp[n+1][m+1];
+        memset(dp,0,sizeof(dp));
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                if(nums1[i-1]==nums2[j-1])
+                {
+                    dp[i][j] = max(dp[i-1][j-1]+1,max(dp[i][j-1],dp[i-1][j]));
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
