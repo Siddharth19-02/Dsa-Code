1a723f34cb4cfc6b4b110b55a3a5580d0d6b70ca
Siddharth19-02 <71065965+Siddharth19-02@users.noreply.github.com>
2024-12-29 21:56:44 +0530

    Create 1746-maximum-subarray-sum-after-one-operation.cpp

---
+++ Dynamic Programming/1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int max(long long int a , long long int b)
+    {
+        return (a>=b)?a:b;
+    }
+    int maxSumAfterOperation(vector<int>& nums) {
+
+        int n = nums.size();
+        
+        int maxSumWithoutSquare = max(0,nums[0]);
+        int maxSumWithSquare = (nums[0] * nums[0]);
+
+        long long int max_sum = max(maxSumWithoutSquare, maxSumWithSquare);
+
+        for(int i=1;i<n;i++)
+        {
+            max_sum = max(max_sum, max(nums[i], nums[i]+maxSumWithSquare) ); 
+
+            maxSumWithSquare = max(nums[i]*nums[i] ,
+                    max(maxSumWithoutSquare + nums[i]*nums[i]
+                , maxSumWithSquare+nums[i]
+                )
+            );
+
+            maxSumWithoutSquare = max(0,max(nums[i], nums[i] + maxSumWithoutSquare) );
+
+            max_sum = max(max_sum, maxSumWithSquare);
+        }
+
+        return max_sum;
+    }
+};
