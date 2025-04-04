3a8ae8f8a593e4469ddc454565212c47056416f1
Siddharth19-02 <71065965+Siddharth19-02@users.noreply.github.com>
2025-04-04 09:31:36 +0530

    Create 1123-smallest-subtree-with-all-the-deepest-nodes.cpp

---
+++ Trees/1123-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<TreeNode* ,int> lca(TreeNode* root)
+    {
+        if(!root)
+            return {NULL, 0};
+
+        pair<TreeNode* ,int> left = lca(root->left);
+        pair<TreeNode* ,int> right = lca(root->right);
+
+        if(left.second > right.second)
+        {
+            return {left.first, left.second + 1};
+        }
+
+        if(left.second < right.second)
+        {
+            return {right.first, right.second + 1};
+        }
+
+        return {root, left.second + 1};
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return lca(root).first;
+    }
+};
+
